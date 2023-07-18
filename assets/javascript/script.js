2a426feb8c0ed5123f4654b3d420f7feb23f6880
oussama ouakassi <lmoutchoo@gmail.com>
2023-07-18 13:42:44 +0100

    delete some js code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,3 @@
-import arrLang from "./data.js";
-
 const swiper = new Swiper(".hero__swiper", {
   //  parameters
   direction: "horizontal",
@@ -38,66 +36,3 @@ window.addEventListener("scroll", () => {
 });
 
 console.log(window.innerHeight);
-
-// ----------- multi languages funcion ---------------- //
-
-const translateLang = (lang) => {
-  // Select all elements with the class "lang"
-  const elements = document.querySelectorAll(".lang");
-
-  console.log(elements);
-
-  // Iterate over each element and update its text content with the translated text
-  elements.forEach((item) => {
-    // console.log(item);
-    // Get the translation key from the "key" attribute of the element
-    const key = item.getAttribute("key");
-
-    console.log(key);
-
-    console.log(arrLang["en"]["services"]["logicielsCard"]["content"]);
-
-    if (key.includes("-")) {
-      // If the key has a dot (.), it represents a nested key for a section's content
-      const [sectionKey, nameKey, contentKey] = key.split("-");
-
-      // Update the text content of the element with the translated section content
-      item.textContent = arrLang[lang][sectionKey][nameKey][contentKey];
-      console.log(item);
-    } else {
-      // Otherwise, it's a direct translation key for an element
-      // Update the text content of the element with the translated text
-      item.textContent = arrLang[lang][key];
-    }
-  });
-};
-
-window.addEventListener("DOMContentLoaded", () => {
-  // Check if a language is already stored in localStorage
-  const storedLang = localStorage.getItem("stored_lang");
-  if (storedLang != null && storedLang != undefined) {
-    const lang = storedLang;
-    // Translate the page to the stored language
-    translateLang(lang);
-  }
-
-  // Select all elements with the class "translate"
-  const translateButtons = document.querySelectorAll(".translate");
-
-  // Attach a click event listener to each translate button
-  translateButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      // Get the language ID from the "id" attribute of the button
-      const lang = this.getAttribute("id");
-
-      // Store the selected language to localStorage
-      localStorage.setItem("stored_lang", lang);
-
-      // Set HTML lang attribute programmatically
-      document.documentElement.setAttribute("lang", lang);
-
-      // Translate the page to the selected language
-      translateLang(lang);
-    });
-  });
-});
