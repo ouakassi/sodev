73452e9e2b8c8bd67e3a9a13ea9c33677d023fdf
oussama ouakassi <lmoutchoo@gmail.com>
2023-07-23 01:59:17 +0100

    add responsive for mobile and tablet

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".hero__swiper", {
+new Swiper(".hero__swiper", {
   //  parameters
   direction: "horizontal",
   loop: true,
@@ -17,22 +17,42 @@ const swiper = new Swiper(".hero__swiper", {
     nextEl: ".hero__swiper-button-next",
     prevEl: ".hero__swiper-button-prev",
   },
-
-  //  scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
 });
 
 // ------------------ to top button --------- //
 
-const toTopElement = document.querySelector(".to__top");
+let toTopElement = document.querySelector(".to__top");
 
-console.log(window.scrollY);
 window.addEventListener("scroll", () => {
   window.scrollY > 100
     ? (toTopElement.style.display = "block")
     : (toTopElement.style.display = "none");
 });
 
-console.log(window.innerHeight);
+// ------------------ responssive navbar --------- //
+
+const showButton = document.querySelector(".show__button");
+const closeButton = document.querySelector(".close__button");
+const header = document.querySelector("header");
+const navbarItems = document.querySelectorAll("nav li");
+
+showButton.addEventListener("click", () => {
+  header.classList.add("active");
+});
+closeButton.addEventListener("click", () => {
+  header.classList.remove("active");
+});
+
+navbarItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    header.classList.remove("active");
+  });
+});
+
+// ------------------ animations  --------- //
+
+const servicesCard = document.querySelector(".card");
+
+gsap.from(servicesCard, { opacity: 0, y: -300, duration: 1, scale: 1.1 });
+
+// gsap.to(servicesCard, { opacity: 1, y: 0, scale: 1 });
