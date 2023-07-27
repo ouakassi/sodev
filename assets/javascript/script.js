e48d32291a892a18db338811f8c4edadc7fe8a97
oussama ouakassi <lmoutchoo@gmail.com>
2023-07-27 12:33:16 +0100

    adding animation and fix some naming

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,7 @@ new Swiper(".hero__swiper", {
   autoplay: {
     delay: 3000,
   },
-  effect: "cards",
+  effect: "fade",
   //  pagination
   pagination: {
     el: ".swiper-pagination",
@@ -49,10 +49,93 @@ navbarItems.forEach((item) => {
   });
 });
 
+// ------------------ loader --------- //
+
+//------------------- Loading content ------------------ //
+
+const spinnerContainer = document.querySelector(".spinner__container");
+const spinner = document.querySelector(".spinner");
+
+function showLoader() {
+  document.getElementById("loader").style.display = "flex";
+}
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  showLoader();
+});
+
+window.addEventListener("load", function (e) {
+  hideLoader();
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll("nav");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 180;
+    const sectionId = current.getAttribute("id");
+    const navLink = document.querySelector(`nav a[href*=${sectionId}]`);
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navLink.classList.add("active__link");
+    } else {
+      navLink.classList.remove("active__link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
 // ------------------ animations  --------- //
 
-// const servicesCard = document.querySelector(".card");
+ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+  delay: 200,
+  viewFactor: 0.1,
+}).reveal(".card , .step__card , .section__heading", {
+  // easing: "cubic-bezier(1,0,0,1)",
+  interval: 600,
+  cleanup: true,
+});
 
-// gsap.from(servicesCard, { opacity: 0, y: -300, duration: 1, scale: 1.1 });
+ScrollReveal({ origin: "top", distance: "100px", delay: 200 }).reveal(
+  ".card p , header  ",
+  {
+    // easing: "cubic-bezier(1,0,0,1)",
+    interval: 600,
+    cleanup: true,
+  }
+);
 
-// gsap.to(servicesCard, { opacity: 1, y: 0, scale: 1 });
+ScrollReveal().reveal(".button");
+
+ScrollReveal({ origin: "top", distance: "20px", delay: 200 }).reveal(
+  " .show__button ",
+  {
+    // easing: "cubic-bezier(1,0,0,1)",
+    cleanup: true,
+  }
+);
+
+ScrollReveal({
+  origin: "bottom",
+  distance: "70px",
+  delay: 300,
+  duration: 1000,
+  opacity: 1,
+  viewFactor: 0.8,
+}).reveal(".steps__wave , .wave__container", {
+  interval: 600,
+
+  cleanup: true,
+});
