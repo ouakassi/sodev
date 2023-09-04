5da40319cbf6f07286861e9d1d5d145c698c85ab
oussama ouakassi <lmoutchoo@gmail.com>
2023-09-04 11:09:31 +0100

    add read more in all pages and translate german pages

--- assets/javascript/services.js
+++ assets/javascript/services.js
@@ -61,3 +61,42 @@ ScrollReveal().reveal(
 //     header.classList.remove("active");
 //   });
 // });
+
+// var textWrapper = document.querySelector(".ml6 .letters");
+// textWrapper.innerHTML = textWrapper.textContent.replace(
+//   /\S/g,
+//   "<span class='letter'>$&</span>"
+// );
+
+// anime.timeline().add({
+//   targets: ".ml6 .letter",
+//   translateY: ["1.1em", 0],
+//   translateZ: 0,
+//   duration: 1200,
+//   // easing: "easeOutExpo",
+//   delay: (el, i) => 100 * i,
+// });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline()
+  .add({
+    targets: ".ml3 .letter",
+    opacity: [0, 1],
+    easing: "easeInOutQuad",
+    duration: 1000,
+    delay: (el, i) => 150 * (i + 1),
+  })
+  .add({
+    targets: ".ml3",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
